f7e8ef51e57e42849c484c109a4b17b3f33b3d1f
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-22 20:58:56 +0200

    Employee controller

--- EmployeeAPI/Controllers/WeatherForecastController.cs
+++ EmployeeAPI/Controllers/WeatherForecastController.cs
@@ -18,8 +18,7 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet]
-    [Route("weather")]
+    [HttpGet("weather")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
